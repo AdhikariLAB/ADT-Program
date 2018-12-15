4d32b4090ef864fc1e47ae6b854705cd884f266e
Koushik Naskar <koushik.naskar9@gmail.com>
2018-12-15 15:58:49 +0530

    adt7 modified

--- ADT1/SRC/adt7.py
+++ ADT1/SRC/adt7.py
@@ -4,9 +4,9 @@
 #This python program is written by Soumya Mukherjee, Bijit Mukherjee, Saikat Mukherjee, Subhankar Sardar and Satrajit Adhikari (corresponding author)
 ######################################################################################################################################################
 
-import ModuleBase
+import ModuleBase, sys
 
-N = 'NO OF STATE'
+N = int(sys.argv[1])
 ND=N*(N-1)/2
 
 #formation of adiabatic to diabatic transformation (ADT) matrix
@@ -22,40 +22,41 @@ R_Matrix = ModuleBase.multiply(ModuleBase.negative(NACM),A_Matrix)
 RHSelems = ModuleBase.elemselect(R_Matrix)
 
 #writing the coefficient matrix of the nonadiabatic coupling terms (NACT)
-fl = open('TAUCOEFF.DAT','w')
-
+txt= ""
 count = 0
 for index1 in range(2,N+1):
   for index2 in range(1,index1):
     count += 1
-    fl.write(' c(%r) = cos(theta(%r,%r))\n' % (count,index2,index1))
-    fl.write('\n')
-    fl.write(' s(%r) = sin(theta(%r,%r))\n' % (count,index2,index1))
-    fl.write('\n')
+    txt += 'c({c}) = cos(theta({i2},{i1}))\n\ns({c}) = sin(theta({i2},{i1}))\n\n'\
+        .format(c=count, i1=index1, i2=index2)
 
-fl.close()
+with open('TAUCOEFF.DAT','w') as f:
+    f.write(txt)
 
-taurow = 0 
-for i in RHSelems:
-    taurow += 1
-    ModuleBase.extracttau(i,N,taurow)
+# taurow = 0 
+# for i in RHSelems:
+#     taurow += 1
+#     ModuleBase.extracttau(i,N,taurow)
+
+for j,i in enumerate(RHSelems,1):
+    ModuleBase.extracttau(i,N,j)
 
 #creating information file
-fl1 = open('INFORMATION.DAT','w')
-fl1.write('\n')
-fl1.write('################################################################################################################################')
-fl1.write('\n')
-fl1.write('#Authors are Soumya Mukherjee, Bijit Mukherjee, Saikat Mukherjee, Subhankar Sardar and Satrajit Adhikari (corresponding author)')
-fl1.write('\n')
-fl1.write('################################################################################################################################')
-fl1.write('\n')
-fl1.write('\n')
-fl1.write('###############################################################################################################')
-fl1.write('\n')
-fl1.write('#The result file contains elements of Coefficient Matrix corresponding to Nonadiabatic Coupling Terms (NACTs).')
-fl1.write('\n')
-fl1.write('###############################################################################################################')
-fl1.write('\n')
-fl1.write('\n')
+info="""
 
-fl1.close()
+################################################################################################################################
+
+#Authors are Soumya Mukherjee, Bijit Mukherjee, Saikat Mukherjee, Subhankar Sardar and Satrajit Adhikari (corresponding author)
+
+################################################################################################################################
+
+
+###############################################################################################################
+
+#The result file contains elements of Coefficient Matrix corresponding to Nonadiabatic Coupling Terms (NACTs).
+
+###############################################################################################################
+
+"""
+with open('INFORMATION.DAT','w') as f:
+    f.write(info)
