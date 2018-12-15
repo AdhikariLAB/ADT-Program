4c12b33ed4659a06155ff8630dfaa62a1ffc6ffd
Koushik Naskar <koushik.naskar9@gmail.com>
2018-12-15 15:36:10 +0530

    adt1 modified

--- ADT1/SRC/adt1.py
+++ ADT1/SRC/adt1.py
@@ -4,41 +4,51 @@
 #This python program is written by Soumya Mukherjee, Bijit Mukherjee, Saikat Mukherjee, Subhankar Sardar and Satrajit Adhikari (corresponding author)
 ######################################################################################################################################################
 
-import ModuleBase
+import ModuleBase, sys
 
-N = 'NO OF STATE'
+N = 
 ND=N*(N-1)/2
 
 #formation of adiabatic potential energy matrix
 U_Matrix = ModuleBase.adiabatic(N)
 
 #writing of adiabatic potential energy matrix elements 
-fl = open('U_MATRIX.DAT','w')
+# fl = open('U_MATRIX.DAT','w')
 
+# for i in range(1,N+1):
+#   for j in range(1,N+1):
+#     fl.write(' U_%r_%r = ' % (i,j))      
+#     E = U_Matrix[i-1][j-1]  
+#     fl.write(E)
+#     fl.write('\n')
+#     fl.write('\n')
+
+
+txt =""
 for i in range(1,N+1):
   for j in range(1,N+1):
-    fl.write(' U_%r_%r = ' % (i,j))      
-    E = U_Matrix[i-1][j-1]  
-    fl.write(E)
-    fl.write('\n')
-    fl.write('\n')
+    txt += ' U_%s_%s = ' % (i,j)
+    txt += U_Matrix[i-1][j-1] +"\n\n"  
 
-#creating information file
-fl1 = open('INFORMATION.DAT','w')
-fl1.write('\n')
-fl1.write('################################################################################################################################')
-fl1.write('\n')
-fl1.write('#Authors are Soumya Mukherjee, Bijit Mukherjee, Saikat Mukherjee, Subhankar Sardar and Satrajit Adhikari (corresponding author)')
-fl1.write('\n')
-fl1.write('################################################################################################################################')
-fl1.write('\n')
-fl1.write('\n')
-fl1.write('#########################################################################')
-fl1.write('\n')
-fl1.write('#The result file contains elements of Adiabatic Potential Energy Matrix.')
-fl1.write('\n')
-fl1.write('#########################################################################')
-fl1.write('\n')
-fl1.write('\n')
+with open('U_MATRIX.DAT', "w") as f:
+    f.write(txt)
 
-fl1.close()
+info="""
+
+################################################################################################################################
+
+#Authors are Soumya Mukherjee, Bijit Mukherjee, Saikat Mukherjee, Subhankar Sardar and Satrajit Adhikari (corresponding author)
+
+################################################################################################################################
+
+
+#########################################################################
+
+#The result file contains elements of Adiabatic Potential Energy Matrix.
+
+#########################################################################
+
+
+"""
+with open('INFORMATION.DAT','w') as f:
+    f.write(info)
