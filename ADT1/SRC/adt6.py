d1cac0d13337945134baa0eeea68cd219a3f8dff
Koushik Naskar <koushik.naskar9@gmail.com>
2018-12-15 15:58:38 +0530

    adt6 modified

--- ADT1/SRC/adt6.py
+++ ADT1/SRC/adt6.py
@@ -4,9 +4,9 @@
 #This python program is written by Soumya Mukherjee, Bijit Mukherjee, Saikat Mukherjee, Subhankar Sardar and Satrajit Adhikari (corresponding author)
 ######################################################################################################################################################
 
-import ModuleBase
+import ModuleBase, sys
 
-N = 'NO OF STATE'
+N = int(sys.argv[1])
 ND=N*(N-1)/2
 
 #formation of adiabatic to diabatic transformation (ADT) matrix
@@ -16,49 +16,53 @@ A_Matrix = ModuleBase.matman(N)
 Aelems = ModuleBase.elemselect(A_Matrix)
 
 #differentiating the collected elements of ADT matrix
-LHSelems = []
-
-for i in range(len(Aelems)):
-    LHSelems.append(ModuleBase.diff(Aelems[i],N))
+# LHSelems = []
+# for i in range(len(Aelems)):
+#     LHSelems.append(ModuleBase.diff(Aelems[i],N))
+LHSelems = [ModuleBase.diff(el,N) for el in Aelems]
 
 #writing the coefficient matrix of the gradient of the ADT angles
-fl = open('GRADCOEFF.DAT','w')
 
+txt= ""
 count = 0
 for index1 in range(2,N+1):
   for index2 in range(1,index1):
     count += 1
-    fl.write(' c(%r) = cos(theta(%r,%r))\n' % (count,index2,index1))
-    fl.write('\n')
-    fl.write(' s(%r) = sin(theta(%r,%r))\n' % (count,index2,index1))
-    fl.write('\n')
+    txt += 'c({c}) = cos(theta({i2},{i1}))\n\ns({c}) = sin(theta({i2},{i1}))\n\n'\
+        .format(c=count, i1=index1, i2=index2)
 
-fl.close()
+with open('GRADCOEFF.DAT','w') as f:
+    f.write(txt)
 
-gradrow = 0 
-for i in LHSelems:
-    gradrow += 1
-    ModuleBase.extractgrad(i,N,gradrow)
 
+
+# gradrow = 0 
+# for i in LHSelems:
+#     gradrow += 1
+#     ModuleBase.extractgrad(i,N,gradrow)
+
+for j,i in enumerate(LHSelems,1):
+    ModuleBase.extractgrad(i,N,j)
 #creating information file
-fl1 = open('INFORMATION.DAT','w')
-fl1.write('\n')
-fl1.write('################################################################################################################################')
-fl1.write('\n')
-fl1.write('#Authors are Soumya Mukherjee, Bijit Mukherjee, Saikat Mukherjee, Subhankar Sardar and Satrajit Adhikari (corresponding author)')
-fl1.write('\n')
-fl1.write('################################################################################################################################')
-fl1.write('\n')
-fl1.write('\n')
-fl1.write('##########################################################################################################################')
-fl1.write('##############')
-fl1.write('\n')
-fl1.write('#The result file contains elements of Coefficient Matrix corresponding to Gradient of Adiabatic to Diabatic Transformation') 
-fl1.write('(ADT) Angles.')
-fl1.write('\n')
-fl1.write('##########################################################################################################################')
-fl1.write('##############')
-fl1.write('\n')
-fl1.write('\n')
 
-fl1.close()
+info = """
+
+################################################################################################################################
+
+#Authors are Soumya Mukherjee, Bijit Mukherjee, Saikat Mukherjee, Subhankar Sardar and Satrajit Adhikari (corresponding author)
+
+################################################################################################################################
+
+
+########################################################################################################################################
+
+#The result file contains elements of Coefficient Matrix corresponding to Gradient of Adiabatic to Diabatic Transformation
+(ADT) Angles.
+
+########################################################################################################################################
+
+"""
+
+
+with open('INFORMATION.DAT','w') as f:
+    f.write(info)
