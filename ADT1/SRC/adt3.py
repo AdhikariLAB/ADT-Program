baff26cf873eccfda061a4f1c12ec3f873d33e4c
Koushik Naskar <koushik.naskar9@gmail.com>
2018-12-15 15:57:46 +0530

    adt3 modified

--- ADT1/SRC/adt3.py
+++ ADT1/SRC/adt3.py
@@ -4,60 +4,51 @@
 #This python program is written by Soumya Mukherjee, Bijit Mukherjee, Saikat Mukherjee, Subhankar Sardar and Satrajit Adhikari (corresponding author)
 ######################################################################################################################################################
 
-import ModuleBase
+import ModuleBase, sys
 
-N = 'NO OF STATE'
+N = int(sys.argv[1])
 ND=N*(N-1)/2
 
 #formation of adiabatic to diabatic transfromation (ADT) matrix 
 A_Matrix = ModuleBase.matman(N)
 
-#writing of ADT matrix elements
-fl = open('A_MATRIX.DAT','w')
 
+txt= ""
 count = 0
 for index1 in range(2,N+1):
   for index2 in range(1,index1):
     count += 1
-    fl.write(' c(%r) = cos(theta(%r,%r))\n' % (count,index2,index1))
-    fl.write('\n')
-    fl.write(' s(%r) = sin(theta(%r,%r))\n' % (count,index2,index1))
-    fl.write('\n')
+    txt += 'c({c}) = cos(theta({i2},{i1}))\n\ns({c}) = sin(theta({i2},{i1}))\n\n'\
+        .format(c=count, i1=index1, i2=index2)
+
 
 for i in range(1,N+1):
   for j in range(1,N+1):
-    fl.write(' A_%r_%r = \n' % (i,j))      
+    txt += ' A_%s_%s = \n' % (i,j)   
     E = A_Matrix[i-1][j-1]  
-    length = len(E)
-    line = length/140                                                                                                                          
-    for k1 in range(1,line+1):
-      start = 0 + (k1-1)*140
-      end = k1*140 
-      segment = E[start:end]
-      fl.write(' ')
-      fl.write(segment)
-      fl.write('\n')
-    fl.write(' ')
-    fl.write(E[line*140:])
-    fl.write('\n')
-    fl.write('\n')
+    txt += "\n".join(E[i:i+140] for i in range(0,len(E),140))+"\n\n"
+
+with open('A_MATRIX.DAT','w') as f:
+    f.write(txt)
+
 
 #creating information file
-fl1 = open('INFORMATION.DAT','w')
-fl1.write('\n')
-fl1.write('################################################################################################################################')
-fl1.write('\n')
-fl1.write('#Authors are Soumya Mukherjee, Bijit Mukherjee, Saikat Mukherjee, Subhankar Sardar and Satrajit Adhikari (corresponding author)')
-fl1.write('\n')
-fl1.write('################################################################################################################################')
-fl1.write('\n')
-fl1.write('\n')
-fl1.write('###################################################################################')
-fl1.write('\n')
-fl1.write('#The result file contains elements of Adiabatic to Diabatic Transformation Matrix.')
-fl1.write('\n')
-fl1.write('###################################################################################')
-fl1.write('\n')
-fl1.write('\n')
+info ="""
 
-fl1.close()
+################################################################################################################################
+
+#Authors are Soumya Mukherjee, Bijit Mukherjee, Saikat Mukherjee, Subhankar Sardar and Satrajit Adhikari (corresponding author)
+
+################################################################################################################################
+
+
+###################################################################################
+
+#The result file contains elements of Adiabatic to Diabatic Transformation Matrix.
+
+###################################################################################
+
+
+"""
+with open('INFORMATION.DAT','w') as f:
+    f.write(info)
