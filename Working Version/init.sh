6ccaf5554dbf21010f8679f1a4b1a9e0bbf31b2c
Koushik Naskar <koushik.naskar9@gmail.com>
2019-01-16 10:55:31 +0530

    ifort support added

--- Working Version/init.sh
+++ Working Version/init.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-f2py -c adt.f90 -m adt_module --f90flags='-fopenmp' -lgomp only: get_angle amat  #with openmp parallelization
+
+# with openmp parallelization, NOTE: 'fopenmp' is a gfortran flag
+f2py -c adt.f90 -m adt_module --f90flags='-fopenmp' -lgomp only: get_angle amat 
+# for ifort, following flags may not work universally
+f2py -c adt.f90 -m adt_module --fcompiler=intelem --f90flags='-qopenmp' -liomp5 only: get_angle amat 
 # f2py -c adt.f90 -m adt_module only: get_angle amat                                 #non-openmp
 echo alias adt='"'python "'$(pwd)/adt_final.py'"  '"'>>~/.bashrc
 source ~/.bashrc
